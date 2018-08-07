7d68b0e87caac5db3555cee909cd1088b18473bb
Ben Corman <benjamincorman@gmail.com>
2018-08-06 20:31:06 -0700

    added user model

--- server.js
+++ server.js
@@ -3,28 +3,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const http = require('http')
+const http = require('http');
 const cors = require('cors');
 const db = require('./models/index.js');
-const router = require('./router')
-const app = express()
+const router = require('./router');
+const mongoose = require('mongoose');
+const app = express();
 
-//Database Initialization
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://localhost:3306/database')
 
 
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ type: '*/*' }))
+app.use(bodyParser.json({ type: '*/*' }));
 app.use(function (err, req, res, next) {
     console.log(err.stack)
     res.status(500).send('What have you done...')
-})
+});
 app.use(function (req, res, next) {
     res.status(404).send('Not found...')
-})
+});
 
 router(app);
 const port = process.env.PORT || 3090;
@@ -34,4 +32,11 @@ server.listen(port, function () {
     console.log('Server connected')
 });
 
-console.log('Server listening on: ', port)
+console.log('Server listening on: ', port);
+
+// Database Initialization
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/tournamented'
+);
