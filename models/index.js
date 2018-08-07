c56784f052368af25406981706ad2d98bfd4e053
bcorman <benjamincorman@gmail.com>
2018-08-06 21:09:18 -0700

    added school, debate, and team models + index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
+const mongoose = require("mongoose")
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/tournamented", { useNewUrlParser: true } )
+
+
 module.exports = {
-    User: require('./user.js')
+    User: require('./user.js'),
+    School: require('./school.js'),
+    Debate: require('./debate.js'),
+    Team: require('./team.js'),
+    Person: require('./person.js')
 }
