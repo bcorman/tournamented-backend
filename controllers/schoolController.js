e09b9706e75170cb655f4e8b7caace1ffa7b7d14
bcorman <benjamincorman@gmail.com>
2018-08-27 21:29:50 -0700

    modified schoolController index method

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -15,26 +15,26 @@ module.exports = {
   index: (req, res) => {
     //get all schools
     db.School.find({}, (err, schools) => {
-        if (err) { console.log(err) }
-        console.log(`Schools Delivered`);
-        res.json(schools);
-      })
+      if (err) { console.log(err) }
+      console.log(`Schools Delivered`);
+      res.json(schools);
+    })
   },
   indexByTour: (req, res) => {
     //get all schools for specific tournament
     const id = req.params.tournamentid;
-    console.log('indexByTour hit')
+    console.log('indexByTour hit');
     db.Tournament.findById(id)
       .populate('schools')
       .exec((err, tournament) => {
-       if (err) {
-         res.sendStatus(500);
-         console.log(err);
-       }
-       const schools = tournament.schools;
+        if (err) {
+          res.sendStatus(500);
+          console.log(err);
+        }
+        const schools = tournament.schools;
 
-       res.json(schools);
-     })
+        res.json(schools);
+      });
   },
   create: (req, res) => {
     const id = req.body.id;
@@ -46,41 +46,41 @@ module.exports = {
         console.log(err);
       }
 
-    db.School.findOne({name: school.name}, (err, found) => {
-      if (err) { console.log(err) }
-      if (found) {
-        res.send('School name must be unique');
-      }
-      if (found === null) {
+      db.School.findOne({name: school.name}, (err, found) => {
+        if (err) { console.log(err) }
+        if (found) {
+          res.send('School name must be unique');
+        }
+        if (found === null) {
           let newSchool = new db.School({
             name: school.name,
             tournaments: [tournament]
-          })
+          });
           tournament.schools.push(newSchool);
           tournament.save();
           newSchool.save();
         }
-      })
-    })
+      });
+    });
   },
   show: (req, res) => {
     //get single school
     let id = req.params.id;
     db.School.findById(id, (err, school) => {
-        if (err) { console.log(err) }
-        console.log(`Single School Delivered`)
-        res.json(school);
-    })
+      if (err) { console.log(err) }
+      console.log(`Single School Delivered`);
+      res.json(school);
+    });
   },
   destroy: (req, res) => {
 
     //User should never need to do this.
-    let id = req.params.id
+    let id = req.params.id;
     db.School.findByIdAndRemove(id, (err, success) => {
       if (err) { console.log(err) }
       console.log(`School removed`);
       res.json(success);
-    })
+    });
   },
   update: (req, res) => {
     let id = req.params.id;
@@ -89,6 +89,6 @@ module.exports = {
       if (err) { console.log(err) }
       console.log(success);
       res.json(success);
-    })
+    });
   }
-}
+};
