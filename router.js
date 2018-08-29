1a920657661530f65c4d11302adc6aae177c9d52
bcorman <benjamincorman@gmail.com>
2018-08-29 14:46:10 -0700

    added create student method and route

--- router.js
+++ router.js
@@ -9,6 +9,7 @@ module.exports = (app) => {
   app.post('/signin', requireSignIn, controller.authentication.signIn);
   app.post('/setup/init', controller.tournament.create);
   app.post('/setup/school/create', controller.school.create);
+  app.post('/setup/student/add', controller.person.create);
   app.get('/setup/school/:tournamentid', controller.school.indexByTour);
   app.get('/school/:id', controller.school.show);
   app.get('/:tournamentid', controller.tournament.show);
