09bdbce858446600bf4de24e2cbab96701ba7a85
Ben Corman <benjamincorman@gmail.com>
2020-10-27 23:34:11 -0700

    Add new handling of env variables

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,20 @@
-const passport = require('passport');
-const User = require('../models/user');
-const config = require('../config');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const LocalStrategy = require('passport-local');
-
+const passport = require("passport");
+const User = require("../models/user");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
 
 // Create local strategy
-const localOptions = {usernameField: 'email'};
-const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function (
+  email,
+  password,
+  done
+) {
   // Verify this email and password, call done with the user
   // if it is correct email and password
   // otherwise call done with false
-  User.findOne({email: email}, function (err, user) {
+  User.findOne({ email: email }, function (err, user) {
     if (err) {
       return done(err);
     }
@@ -27,18 +29,16 @@ const localLogin = new LocalStrategy(localOptions, function (email, password, do
         return done(null, false);
       }
       return done(null, user);
-    })
+    });
     // compare passwords - is `password` equal to user.password?
-  })
-
+  });
 });
 
-
 // Setup options for JWT strategy
-
+console.log(process.env.key);
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: config.secret || process.env.KEY
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: process.env.KEY,
 };
 
 // Create jwt strategy
@@ -59,7 +59,6 @@ const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
     }
   });
 });
-
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
